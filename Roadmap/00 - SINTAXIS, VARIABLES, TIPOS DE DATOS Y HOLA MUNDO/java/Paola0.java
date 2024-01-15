c1dace5ef54666917aa329292fd14ddcaeb5b58e
Paola Correa <90867048+paocorrea@users.noreply.github.com>
2024-01-15 13:40:05 +0100

    Create Paola0.java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/Paola0.java
@@ -0,0 +1,32 @@
+//Página oficial https://www.java.com/
+
+//comentarios en una línea
+
+/*comentarios 
+en varias líneas*/
+
+//variable , se debe declarar el tipo de variable 
+int mi_variable=1;
+int miVariable;
+
+//constante , se utiliza la palabra "final" al inicio 
+final int CONSTANTE=5;
+
+//datos primitivos 
+
+//enteros
+int enteroI=5;
+//decimales
+double quinceDeci=4.5588888855;
+float sieteDeci=4.536574;
+//datos lógicos true or false
+boolean prueba=false;
+//caracteres
+char miCaracter="P";
+
+//imprimir por pantalla
+System.out.println("hola, Java");
+
+
+
+
